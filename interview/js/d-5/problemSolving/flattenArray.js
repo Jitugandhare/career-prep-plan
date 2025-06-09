b787c435a3d29e0edce879f7e7beb4940afa8fbe
Jitu gandhare <jitugandhare@gmail.com>
2025-06-10 00:19:15 +0530

    flatten the array

--- interview/js/d-5/problemSolving/flattenArray.js
+++ interview/js/d-5/problemSolving/flattenArray.js
@@ -2,12 +2,13 @@ function flattenArray(arr) {
     let res=[]
     for(let i of arr){
         if(Array.isArray(i)){
-            res.push(...i)
+            res.push(...flattenArray(i))
         }else{
             res.push(i)
         }
     }
     return res;
 }
-let arr = [1, [2, 3], 4, [5, 6]];
+let arr = [1, [2, [3, 4]], 5];
+
 console.log(flattenArray(arr))
