a19926bc642d41f86fdcc8f16fe368445dbe4e3e
Jitu gandhare <jitugandhare@gmail.com>
2025-06-28 09:30:33 +0530

    praactice

--- closures/outputbased/ex1.js
+++ closures/outputbased/ex1.js
@@ -7,3 +7,18 @@ for (let i = 0; i < 5; i++) {
         console.log(i)
     }, 100)
 }
+
+// before let we are using iife and bind
+
+for(var i=0;i<5;i++){
+    (function(i){
+        setTimeout(()=>{
+            console.log(i)
+        },100)
+    })(i)
+}
+
+
+for(var i=0;i<5;i++){
+    setTimeout(console.log.bind(null,i),100)
+}
