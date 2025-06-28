2603edcdd67911a4a03fec0315151aa804473259
Jitu gandhare <jitugandhare@gmail.com>
2025-06-28 19:29:39 +0530

    practice

---
+++ closures/outputbased/counter.js
@@ -0,0 +1,30 @@
+function counter() {
+    let count = 0;
+
+    return {
+        increment: function () {
+            count++;
+            return count;
+        },
+
+        decrement: function () {
+            count--;
+            return count;
+        },
+        reset: function () {
+            count = 0;
+            return count;
+        }
+    }
+}
+
+
+let c=counter();
+console.log(c.increment())
+console.log(c.increment())
+console.log(c.increment())
+console.log(c.increment())
+console.log(`decrement`)
+console.log(c.decrement())
+console.log(c.decrement())
+console.log(`reset`,c.reset())
