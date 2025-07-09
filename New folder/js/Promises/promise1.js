a48831cdb52ea78bd5ffa6c158cb9a9c85dcb660
Jitu gandhare <jitugandhare@gmail.com>
2025-07-09 18:58:28 +0530

    Promises problem-1

--- New folder/js/Promises/promise1.js
+++ New folder/js/Promises/promise1.js
@@ -1,15 +1,21 @@
-const promise=new Promise((resolve,reject)=>{
-    let success=true;
-    if(success){
+// promise in js is an object that represents eventual completion (or failure) of asynchronous operation and 
+// resulting its value. Promises provides more cleaner and manageable way to work with asynchronous operations.
+
+// it has 3 states: 1.pending, 2.fulfilled,3.rejected.
+
+
+const promise = new Promise((resolve, reject) => {
+    let success = true;
+    if (success) {
         resolve("Operation was successful")
-    }else{
+    } else {
         reject("Operation failed")
     }
 })
 
 
-promise.then((result)=>{
+promise.then((result) => {
     console.log(result)
-}).catch((error)=>{
+}).catch((error) => {
     console.log(error)
 })
