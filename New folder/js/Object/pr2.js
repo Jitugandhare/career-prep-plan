f1cc769fdd22b6e28d6a90327d0cf7bcf243970f
Jitu gandhare <jitugandhare@gmail.com>
2025-07-15 11:16:12 +0530

    object practice questions

---
+++ New folder/js/Object/pr2.js
@@ -0,0 +1,25 @@
+// Create an array of objects where each object represents a student with id, name, and score.
+
+const students = [
+    { id: 1, name: "jitu", score: 500 },
+    { id: 2, name: "jay", score: 450 },
+    { id: 3, name: "ravi", score: 400 },
+    { id: 4, name: "john", score: 300 },
+]
+
+//Write a function to find a student by their id from the array.
+
+function findById(id){
+    return students.map((i)=>{
+       
+            if(i.id===id){
+                return i
+            }else{
+                return null
+            }
+        
+    });
+    
+}
+
+console.log(findById(1))
