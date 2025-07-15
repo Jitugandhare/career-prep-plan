8463fa13bfe2f4d91061f5022b6f8869ec7071ca
Jitu gandhare <jitugandhare@gmail.com>
2025-07-15 11:19:44 +0530

    object practice questions

--- New folder/js/Object/pr2.js
+++ New folder/js/Object/pr2.js
@@ -9,6 +9,24 @@ const students = [
 
 //Write a function to find a student by their id from the array.
 
+
+//using map
+function findByIde(id){
+    return students.map((i)=>{
+       
+            if(i.id===id){
+                return i
+            }else{
+                return null
+            }
+        
+    });
+    
+}
+
+console.log(findByIde(1))
+
+// find method
 function findById(id){
     return students.find((i)=>i.id===id)
     
