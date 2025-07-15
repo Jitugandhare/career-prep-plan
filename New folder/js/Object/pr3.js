e61030b03f78116849d132fd229620a2a942a956
Jitu gandhare <jitugandhare@gmail.com>
2025-07-15 11:32:30 +0530

    object practice questions

--- New folder/js/Object/pr3.js
+++ New folder/js/Object/pr3.js
@@ -1,9 +1,9 @@
 
 const students = [
     { id: 1, name: "jitu", score: 500 },
-    { id: 2, name: "jay", score: 450 },
-    { id: 3, name: "ravi", score: 400 },
-    { id: 4, name: "john", score: 300 },
+    { id: 2, name: "jay", score: 400 },
+    { id: 3, name: "ravi", score: 450 },
+    { id: 4, name: "john", score: 350 },
 ]
 
 // average score of all
@@ -16,3 +16,13 @@ function avg(array) {
 }
 
 console.log(`Average score:` ,avg(students))
+
+
+// Sort the array of students by their score in descending order.
+
+function sortStudents(arr){
+    let ans= [...arr].sort((a,b)=>b.score-a.score)
+    return ans;
+}
+
+console.log(`Sort the array of students by their score in descending order:` ,sortStudents(students))
