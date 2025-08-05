ac7f2f5e3f071699b61ce65cde63683ede2b54a9
Jitu gandhare <jitugandhare@gmail.com>
2025-08-05 22:20:13 +0530

    Provlem solvig

--- Problem solving/q4.js
+++ Problem solving/q4.js
@@ -1,15 +1,21 @@
-function capitalFirstLetterOfStr(str){
-   
-    let ans=str.split(' ');
-    let bag="";
-    for(let i=0;i<ans.length;i++){
-        bag+=ans[i][0].toUpperCase()+ans[i].slice(1)+" "
+function capitalFirstLetterOfStr(str) {
 
-    }
-    console.log(bag)
+    // let ans = str.split(' ');
+    // let bag = "";
+    // for (let i = 0; i < ans.length; i++) {
+    //     bag += ans[i][0].toUpperCase() + ans[i].slice(1) + " "
+
+    // }
+    // console.log(bag)
+
+    let words=str.split(" ");
+    let res=words.map(i=>i.charAt(0).toUpperCase()+i.slice(1)+" ").join(" ")
+
+
+    console.log(res)
 }
 
 
-let str="my name is jitu";
+let str = "my name is jitu";
 
 capitalFirstLetterOfStr(str)
