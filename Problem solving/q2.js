a83e59ca841191f80877a104e91acde269fb48bd
Jitu gandhare <jitugandhare@gmail.com>
2025-08-05 10:14:11 +0530

    Provlem solvig

---
+++ Problem solving/q2.js
@@ -0,0 +1,14 @@
+function flattenArray(arr){
+    let res=[]
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            res=res.concat(flattenArray(arr[i]))
+        }else{
+            res.push(arr[i])
+        }
+    }return res;
+    
+}
+
+let arr=[1, [2, 3], 4, [5, 6]];
+console.log(flattenArray(arr))
